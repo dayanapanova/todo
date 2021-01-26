dac225b56147b73c870dfaa5dba8e3f3f0ffcd48
Dayana Panova <dzopino@abv.bg>
2021-01-26 22:24:46 +0200

    user authentication

--- script.js
+++ script.js
@@ -8,6 +8,7 @@ const activeMarker = document.getElementById("active-marker");
 const loginForm = document.getElementById("login-form");
 const registerForm = document.getElementById("register-form");
 const submitRegisterBtn = document.getElementById("submit-register-btn");
+const submitLoginBtn = document.getElementById("submit-login-btn");
 
 const login = () => {
     registerForm.style.left = "-400px";
@@ -21,20 +22,33 @@ const register = () => {
     activeMarker.style.left = "110px";
 }
 
+const checkUser = (ev) => {
+    ev.preventDefault();
+    const userData = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEYS.userData));
+    console.log(userData);
+    const loginEmail = document.getElementById("login-email").value;
+    const loginPassword = document.getElementById("login-password").value;
+    if(loginEmail === userData.email && loginPassword === userData.password) {
+        console.log("success login")
+    } else {
+        console.log("did not success");
+    }
+}
+
 const setUserData = (ev) => {
     ev.preventDefault();
-    let formData = {
+    const userData = {
         firstname:document.getElementById('register-firstName').value,
         lastname:document.getElementById('register-lastName').value,
         email:document.getElementById('register-email').value,
         password:document.getElementById('register-password').value
     }
-    localStorage.setItem(LOCALSTORAGE_KEYS.userData,JSON.stringify(formData));
-    console.log(localStorage.getItem(LOCALSTORAGE_KEYS.userData)); 
+    localStorage.setItem(LOCALSTORAGE_KEYS.userData,JSON.stringify(userData)); 
 };
 
 loginBtn.onclick = login;
 registerBtn.onclick = register;
 submitRegisterBtn.onclick = setUserData;
+submitLoginBtn.onclick = checkUser;
 
 
